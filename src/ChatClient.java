c71cee8d7bac39df5a2d6263f24695c15136f9b2
iw72 <iw72@pc7-077-l.cs.st-andrews.ac.uk>
2024-11-16 17:15:44 +0000

    Initial server model to handle connections

---
+++ src/ChatClient.java
@@ -0,0 +1,9 @@
+public class ChatClient {
+	private String serverAddress;
+	private int serverPort;
+
+	public void ChatClient(address, port) {
+		serverAddress = address;
+		serverPort = port;
+	}
+}
---
+++ src/ChatClientMain.java
@@ -0,0 +1,10 @@
+public class ChatClientMain {
+	public static void main(String args[]) {
+		validateArgs(args);
+		ServerConnection serverConnection = new 	
+	}
+	
+	private void validateArgs(String args[]) {
+		//TODO
+	}
+}
